023789c9aecef1aa336d7cff3c19ebe09c508e8a
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2021-12-13 01:31:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 userbot = {
 Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
- owner: [ "6281261324817", "62895622934562", "6285855396553", "6285934360746" ],
+ owner: [ "6282320830684", "682320830684", "6282320830684", "6285934360746" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
@@ -34,8 +34,8 @@ Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
    eror: "Sepertinya Eror",
    waits: "Sedang di proses",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU',
-   packname: "RIXLE-BOT-V3",
-   author: 'Arifi Razzaq',
+   packname: "BOTCAHVBOTZ",
+   author: '.',
     setting: {
     admin: "only admin",
     group: "only group",
